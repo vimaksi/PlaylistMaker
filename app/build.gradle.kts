a4f2f5d62dc9d1344d75d36e74156b41b23a7e94
vmaksi <victoria_maksimova@mail.ru>
2025-10-06 23:30:00 +0300

    Init Commit

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -5,7 +5,7 @@ plugins {
 
 android {
     namespace = "com.practicum.playlistmaker"
-    compileSdk = 35
+    compileSdk = 36
 
     defaultConfig {
         applicationId = "com.practicum.playlistmaker"

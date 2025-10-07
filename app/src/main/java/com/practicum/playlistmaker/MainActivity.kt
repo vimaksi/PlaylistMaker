1e50cc4f5dd25e2558d9677c3e250fff9e1a8f33
vmaksi <victoria_maksimova@mail.ru>
2025-10-08 01:30:32 +0300

    Init Commit

--- app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/MainActivity.kt
@@ -1,7 +1,8 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
+import android.widget.TextView
+import android.widget.Toast
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
@@ -14,6 +15,18 @@ class MainActivity : AppCompatActivity() {
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
+        }
+        val search = findViewById<TextView>(R.id.search)
+        val library = findViewById<TextView>(R.id.library)
+        val settings = findViewById<TextView>(R.id.settings)
+        search.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на картинку 1!", Toast.LENGTH_SHORT).show()
+        }
+        library.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на картинку 2!", Toast.LENGTH_SHORT).show()
+        }
+        settings.setOnClickListener {
+            Toast.makeText(this@MainActivity, "Нажали на картинку 3!", Toast.LENGTH_SHORT).show()
         }
     }
 }

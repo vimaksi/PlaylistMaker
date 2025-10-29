c43100590729ca0287205df1fb6be7a94264f559
vimaksi <victoria_maksimova@mail.ru>
2025-10-30 01:45:03 +0300

    Resolve conflict

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,20 +1,89 @@
 package com.practicum.playlistmaker
 
 import android.os.Bundle
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
+import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import android.widget.LinearLayout
+import android.widget.EditText
+import android.widget.ImageView
+import android.text.TextWatcher
+import android.view.View
+import android.text.Editable
+import android.widget.FrameLayout
 
 class SearchActivity : AppCompatActivity() {
+    private var constTextEdit: String = TEXT_EDIT_VALUE
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        enableEdgeToEdge()
+        //enableEdgeToEdge()
         setContentView(R.layout.activity_search)
-        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main)) { v, insets ->
+
+        if (savedInstanceState != null) {
+            constTextEdit = savedInstanceState.getString(EDIT_TEXT, TEXT_EDIT_VALUE)
+        }
+        ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.back)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        setContentView(R.layout.activity_search)
+
+        val main = findViewById<Toolbar>(R.id.back)
+        main.setOnClickListener{
+            finish()
+        }
+
+        val frameLayout = findViewById<FrameLayout>(R.id.container)
+        val inputEditText = findViewById<EditText>(R.id.inputEditText)
+        val clearButton = findViewById<ImageView>(R.id.clearIcon)
+
+        inputEditText.setText(constTextEdit.toString())
+
+        clearButton.setOnClickListener {
+            inputEditText.setText("")
+        }
+
+        val simpleTextWatcher = object : TextWatcher {
+            override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
+                // empty
+            }
+
+            override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
+                constTextEdit = inputEditText.text.toString()
+                clearButton.visibility = clearButtonVisibility(s)
+            }
+
+            override fun afterTextChanged(s: Editable?) {
+            }
+        }
+        inputEditText.addTextChangedListener(simpleTextWatcher)
+
+
+    }
+
+    private fun clearButtonVisibility(s: CharSequence?): Int {
+        return if (s.isNullOrEmpty()) {
+            View.GONE
+        } else {
+            View.VISIBLE
+        }
+    }
+
+    override fun onSaveInstanceState(outState: Bundle) {
+        super.onSaveInstanceState(outState)
+        outState.putString(EDIT_TEXT, constTextEdit)
+    }
+
+    companion object {
+        const val EDIT_TEXT = "EDIT_TEXT"
+        const val TEXT_EDIT_VALUE = ""
+    }
+
+    override fun onRestoreInstanceState(savedInstanceState: Bundle) {
+        super.onRestoreInstanceState(savedInstanceState)
+        constTextEdit = savedInstanceState.getString(EDIT_TEXT, TEXT_EDIT_VALUE)
     }
 }

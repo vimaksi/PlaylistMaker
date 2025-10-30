93bbaf9b46e3c6fcd3c719800f9b924af2aa1d0c
vimaksi <victoria_maksimova@mail.ru>
2025-10-30 22:21:11 +0300

    fix critical comments and small edit

--- app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
+++ app/src/main/java/com/practicum/playlistmaker/SearchActivity.kt
@@ -1,20 +1,24 @@
 package com.practicum.playlistmaker
 
+import android.content.Context
 import android.os.Bundle
+import android.text.Editable
+import android.text.TextWatcher
+import android.view.View
+import android.view.inputmethod.InputMethodManager
+import android.widget.EditText
+import android.widget.FrameLayout
+import android.widget.ImageView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.appcompat.widget.Toolbar
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
-import android.widget.LinearLayout
-import android.widget.EditText
-import android.widget.ImageView
-import android.text.TextWatcher
-import android.view.View
-import android.text.Editable
-import android.widget.FrameLayout
+import androidx.core.view.isVisible
+
 
 class SearchActivity : AppCompatActivity() {
     private var constTextEdit: String = TEXT_EDIT_VALUE
+    private var constIsClearButtonVisible : Int = 8
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         //enableEdgeToEdge()
@@ -22,6 +26,7 @@ class SearchActivity : AppCompatActivity() {
 
         if (savedInstanceState != null) {
             constTextEdit = savedInstanceState.getString(EDIT_TEXT, TEXT_EDIT_VALUE)
+            constIsClearButtonVisible = savedInstanceState.getInt(IS_VISIBLE_BUTTON, 0 )
         }
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.back)) { v, insets ->
             val systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars())
@@ -41,11 +46,15 @@ class SearchActivity : AppCompatActivity() {
         val clearButton = findViewById<ImageView>(R.id.clearIcon)
 
         inputEditText.setText(constTextEdit.toString())
+        clearButton.visibility = constIsClearButtonVisible
 
         clearButton.setOnClickListener {
             inputEditText.setText("")
+            this.currentFocus?.let { view ->
+                val imm = getSystemService(Context.INPUT_METHOD_SERVICE) as? InputMethodManager
+                imm?.hideSoftInputFromWindow(view.windowToken, 0)
+            }
         }
-
         val simpleTextWatcher = object : TextWatcher {
             override fun beforeTextChanged(s: CharSequence?, start: Int, count: Int, after: Int) {
             }
@@ -53,14 +62,13 @@ class SearchActivity : AppCompatActivity() {
             override fun onTextChanged(s: CharSequence?, start: Int, before: Int, count: Int) {
                 constTextEdit = inputEditText.text.toString()
                 clearButton.visibility = clearButtonVisibility(s)
+                constIsClearButtonVisible = clearButton.visibility
             }
 
             override fun afterTextChanged(s: Editable?) {
             }
         }
         inputEditText.addTextChangedListener(simpleTextWatcher)
-
-
     }
 
     private fun clearButtonVisibility(s: CharSequence?): Int {
@@ -74,15 +82,18 @@ class SearchActivity : AppCompatActivity() {
     override fun onSaveInstanceState(outState: Bundle) {
         super.onSaveInstanceState(outState)
         outState.putString(EDIT_TEXT, constTextEdit)
-    }
-
-    companion object {
-        const val EDIT_TEXT = "EDIT_TEXT"
-        const val TEXT_EDIT_VALUE = ""
+        outState.putInt(IS_VISIBLE_BUTTON,constIsClearButtonVisible)
     }
 
     override fun onRestoreInstanceState(savedInstanceState: Bundle) {
         super.onRestoreInstanceState(savedInstanceState)
         constTextEdit = savedInstanceState.getString(EDIT_TEXT, TEXT_EDIT_VALUE)
+        constIsClearButtonVisible = savedInstanceState.getInt(IS_VISIBLE_BUTTON,0)
+    }
+
+    companion object {
+        private const val EDIT_TEXT = "EDIT_TEXT"
+        private const val TEXT_EDIT_VALUE = ""
+        private const val IS_VISIBLE_BUTTON = "IS_VISIBLE_BUTTON"
     }
 }

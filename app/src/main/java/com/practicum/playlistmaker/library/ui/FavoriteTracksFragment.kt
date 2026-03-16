85df611ce4d8dacb2dbfa916eec39b31cd24d66c
Максимова Виктория <victoria_maksimova@mail.ru>
2026-03-16 17:18:17 +0300

    fix tabtheme

--- app/src/main/java/com/practicum/playlistmaker/library/ui/FavoriteTracksFragment.kt
+++ app/src/main/java/com/practicum/playlistmaker/library/ui/FavoriteTracksFragment.kt
@@ -19,7 +19,6 @@ class FavoriteTracksFragment : Fragment() {
         binding = FragmentFavoriteTracksBinding.inflate(inflater, container, false)
         return binding.root
     }
-
     override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
         super.onViewCreated(view, savedInstanceState)
     }
